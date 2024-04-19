e2f75ef48f3e8b35f21be1390d06f963b051ca5a
Graeme Foster <graemefoster@microsoft.com>
2024-04-19 10:04:01 +0800

    Latest version

--- Spa/SantaLite/src/msalConfig.ts
+++ Spa/SantaLite/src/msalConfig.ts
@@ -3,7 +3,7 @@ import { PublicClientApplication } from "@azure/msal-browser";
 export const msalConfig = {
   auth: {
     authority: "https://login.microsoftonline.com/a47f213f-c3ec-49ff-92a1-52d7b0d29e6c",
-    clientId: "f95a88e8-43b7-45fc-ae55-3801040587bf"
+    clientId: "cf26fe55-80a7-4ec6-88fb-4da4aa8ca1ef"
   },
 
   cache: {
